f7a73954d93970af56b795a556c6eeb4163e7a4f
LJ <juranek.lukasz@gmail.com>
2021-06-15 22:25:19 +0200

    Update example fo 08H222

--- tools/IO-LINK-PythonTools/ExampleReadDataFromSensor.py
+++ tools/IO-LINK-PythonTools/ExampleReadDataFromSensor.py
@@ -2,15 +2,37 @@ from IOLinkTools import IOLink, IOLinkFrame
 import time
 
 def newframe_handler(io_link_frame):
-    print("new data rx")
-    print(io_link_frame.FrameType)
-    print(io_link_frame.FrameData)
+    print("new data rx", end=' ')
+    print(io_link_frame.FrameData.hex(), end='\n')
 
 
+iolink = IOLink('COM43',newframe_handler)
 
-iolink = IOLink('/dev/ttyS2',newframe_handler)
-iolink.sendData((b'\x01\x00\x00\x01\x0A\x00\x00\x00'))
-
+iolink.sendData((b'\x01\x00\x00\x01\x0C\x00\x00\x00')) #WAKEUP
 time.sleep(2)
 
-input("Press Enter to continue...")
+## read device name
+iolink.sendData((b'\x01\x00\x00\x01\x01\x00\x00\x00\x70\x93\x10\x83\x00\x00\x00\x00\x00')) #  request read from 0x10 (Name of device)  
+time.sleep(0.5)
+iolink.sendData((b'\x01\x00\x00\x01\x01\x00\x00\x00\xF0')) #request of data read 
+time.sleep(0.5)
+iolink.sendData((b'\x01\x00\x00\x01\x01\x00\x00\x00\xF0')) #request of data read 
+time.sleep(0.5)
+iolink.sendData((b'\x01\x00\x00\x01\x01\x00\x00\x00\xE1')) #request of data read 
+time.sleep(0.5)
+iolink.sendData((b'\x01\x00\x00\x01\x01\x00\x00\x00\xE2')) #request of data read 
+time.sleep(0.5)
+iolink.sendData((b'\x01\x00\x00\x01\x01\x00\x00\x00\xF1')) #request of data read end
+time.sleep(0.5)
+
+
+## read cyclic data
+iolink.sendData((b'\x01\x00\x00\x01\x01\x00\x00\x00\x20\x99\x00\x00\x00\x00\x00\x00\x00')) #request read cyclic data, device state
+time.sleep(0.5)
+while True:
+    iolink.sendData((b'\x01\x00\x00\x01\x02\x00\x00\x00\xF1'))
+    time.sleep(0.5)
+
+## end
+
+# input("Press Enter to continue...")
